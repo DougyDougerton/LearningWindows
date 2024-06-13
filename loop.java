12329d3f48c00dd2dfb652e89558840c4b5e9972
DougyDougerton <douglas.spicer97@hotmail.com>
2024-06-12 20:20:00 -0600

    Pushing my java file

---
+++ loop.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class loop {
+    
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            System.out.println();
+            
+    }
+}
+    }

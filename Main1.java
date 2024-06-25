fde82140ba6393239d9753891fc1696d8f0846ce
DougyDougerton <douglas.spicer97@hotmail.com>
2024-06-25 16:28:33 -0600

    Pushing a 2nd file from VSCode

---
+++ Main1.java
@@ -0,0 +1,13 @@
+public class Main1 {
+    public static void main(String[] args) {
+        //int[] result = {1,2,3,4,5,6,7,8,9,10};
+        int i,j;
+
+        for(i=1;i<6;i++) {
+            for(j=1;j<11;j++){
+
+                System.out.println(i*j);
+            }
+        }
+    }
+}

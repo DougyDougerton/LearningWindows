2c946726ac92cb2baa7117a131db70aeaec51c5d
DougyDougerton <douglas.spicer97@hotmail.com>
2024-08-07 19:45:13 -0600

    Just a simple loop to practice pushing code to github :)

--- loop.java
+++ loop.java
@@ -1,9 +1,8 @@
-import javax.swing.JOptionPane;
 public class loop {
     
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i++) {
-            System.out.println();
+        for(int i = 1; i < 10; i++) {
+            System.out.println(i);
             
     }
 }
